aff1413688a1700e3ca077833cd8c7fb25eaa63d
mosteligible <mosteligible@mosteligible>
2024-04-21 21:23:20 -0400

    add workflow

--- scripts/prep-model.sh
+++ scripts/prep-model.sh
@@ -2,6 +2,6 @@
 
 echo "Getting model from: https://huggingface.co/${HF_MODEL}"
 git lfs install
-git clone "https://huggingface.co/${HF_MODEL}" /srv
+git clone "https://huggingface.co/${HF_MODEL}" /srv/${LLM_MODEL_NAME}
 
 exec "$@"
