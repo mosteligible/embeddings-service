85022d82f1b880019a0617ade4fa0a4d71b0d962
mosteligible <mosteligible@mosteligible>
2024-04-21 22:18:44 -0400

    no-preload test for timeout

--- scripts/cmd.sh
+++ scripts/cmd.sh
@@ -2,7 +2,7 @@
 
 num_processes=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
 
-gunicorn main:app --preload \
+gunicorn main:app \
     --workers "${num_processes}" \
     --worker-class uvicorn.workers.UvicornWorker \
     --bind 0.0.0.0:8000
