bfac69ffcd511f69ce89df09c853302b12d84023
mosteligible <mosteligible@mosteligible>
2024-04-21 21:43:22 -0400

    copy memory in process fork

--- scripts/cmd.sh
+++ scripts/cmd.sh
@@ -2,7 +2,7 @@
 
 num_processes=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
 
-gunicorn main:app \
+gunicorn main:app --preload \
     --workers "${num_processes}" \
     --worker-class uvicorn.workers.UvicornWorker \
     --bind 0.0.0.0:8000
